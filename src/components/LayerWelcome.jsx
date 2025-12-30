970ec64f67f94c3f5ae20a8ae7fee4709ee95a97
Gabby Zapata <gabby.ecuador@gmail.com>
2025-12-30 14:55:56 -0500

    Se comenta el objeto

--- src/components/LayerWelcome.jsx
+++ src/components/LayerWelcome.jsx
@@ -1,3 +1,10 @@
+/**
+ * Creado por: Gabby Zapata
+ * Fecha: 2025-12-30
+ * Descripción: Componente que contiene el efecto del Welcome para la primera visualización
+ * @returns componente LayerWelcome
+ */
+
 import { Spinner } from "react-bootstrap";
 
 export const LayerWelcome = ({ aparece }) => {
